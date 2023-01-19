df5dacdb88310cdf997a5d22a0cd9dc58fb0e661
Sergio De Luca <sdlbsso@gmail.com>
2023-01-19 15:02:56 -0300

    Configuración inicial de program.cs y startup

--- Program.cs
+++ Program.cs
@@ -1,17 +1,14 @@
+using tickets;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+var startup = new StartUp(builder.Configuration);
 
-builder.Services.AddControllers();
+startup.ConfigureServices(builder.Services);
+
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
+startup.Configure(app, app.Environment);
 
 app.Run();
---
+++ StartUp.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace tickets
+{
+    public class StartUp
+    {
+
+
+        public StartUp(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+
+        public void ConfigureServices(IServiceCollection services)
+        { 
+            services.AddControllers();
+            services.AddEndpointsApiExplorer();        
+        }
+
+            public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseCors();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+
+        }
+
+
+    }
+}
